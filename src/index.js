f317d58da3ef350c1e66389c323a05f05fdc1b56
dielitonfonseca <dielitonskt@gmail.com>
2022-04-25 16:38:45 -0300

    requests ok

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-import express from "express";
+import express,{json} from "express";
+import cors from 'cors';
+let users =[
 
+]
+let tweets=[
+
+
+]
 const server = express();
+server.use(cors());
+server.use(json());
 
 server.listen(5000,()=>{console.log("Running . . .")});
+
+server.get('/',(req,res)=>{
+    res.send("API, SALVE!")
+})
+
+server.post('/sign-up',(req,res)=>{
+
+    if(req.body.username===""|| req.body===null||req.body===""){
+        res.status(404).send('Sorry cant find that!');
+    }
+    else{
+        users.push(req.body)
+        console.log(users)
+        res.send("OK")
+    }
+
+})
+server.post('/tweets',(req,res)=>{
+    let userSend = users.find(element => element.username ===req.body.username);
+    tweets.push({"username":req.body.username,"avatar":userSend.avatar,"tweet":req.body.tweet})
+    res.send("OK")
+})
+server.get('/tweets',(req,res)=>{
+    if(tweets.length<=10){
+        res.send(tweets.slice(0).reverse())
+    }
+    else{
+    res.send(tweets.slice(tweets.length-10).slice(0).reverse())
+    }
+})
